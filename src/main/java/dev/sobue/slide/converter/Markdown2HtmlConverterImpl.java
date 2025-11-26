2d7359aa212f9c4421bf922dfb0952396a1921a1
SOBUE Sho <sho@sobue.dev>
2025-11-26 20:29:50 +0900

    Migrate Spring Boot 4.0.0

--- src/main/java/dev/sobue/slide/converter/Markdown2HtmlConverterImpl.java
+++ src/main/java/dev/sobue/slide/converter/Markdown2HtmlConverterImpl.java
@@ -3,13 +3,11 @@ package dev.sobue.slide.converter;
 import java.net.URI;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestClient;
 
 import static java.util.Collections.singletonMap;
-import static org.springframework.http.RequestEntity.post;
 
 /**
  * Implementation for Converter: HTML from Markdown.
@@ -22,7 +20,7 @@ public class Markdown2HtmlConverterImpl implements Markdown2HtmlConverter {
   /**
    * REST Client.
    */
-  private final RestOperations restOperations;
+  private final RestClient restClient;
 
   /**
    * GitHub API endpoint.
@@ -36,9 +34,9 @@ public class Markdown2HtmlConverterImpl implements Markdown2HtmlConverter {
    * @param apiEndPoint API endpoint.
    */
   public Markdown2HtmlConverterImpl(
-      RestTemplateBuilder builder,
+      RestClient.Builder builder,
       @Value("${github.api.markdown:https://api.github.com/markdown}") String apiEndPoint) {
-    this.restOperations = builder.build();
+    this.restClient = builder.build();
     this.apiEndPoint = URI.create(apiEndPoint);
   }
 
@@ -47,12 +45,12 @@ public class Markdown2HtmlConverterImpl implements Markdown2HtmlConverter {
    */
   @Override
   public String convert2html(@NonNull final String markdown) {
-    return restOperations
-        .exchange(
-            post(apiEndPoint)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(singletonMap("text", markdown)),
-            String.class)
-        .getBody();
+    return restClient
+        .post()
+        .uri(apiEndPoint)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(singletonMap("text", markdown))
+        .retrieve()
+        .body(String.class);
   }
 }
